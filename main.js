fb4182fdc637f4ecd82e95c1ab0951b64644bc4a
Jacopo Bufalino <jacopo.bufalino@vaana.fi>
2021-09-30 11:04:08 +0300

    Update logging

--- main.js
+++ main.js
@@ -11,10 +11,9 @@ async function main(){
 
     const ip_response = await axios.get('https://api.ipify.org?format=json')
     const ip = ip_response.data.ip
-    console.log(ip)
     const dns_url = `https://${username}:${password}@domains.google.com/nic/update?hostname=${domain_name}&myip=${ip}`
     const dns_response = await axios.post(dns_url)
-    console.log(dns_response.data)
+    console.log(`Updating DDNS A entry for ${domain_name} => ${dns_response.data}`)
 
 }
 
